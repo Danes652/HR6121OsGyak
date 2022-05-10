f1f69f61a39076486e213b9b2f3d648ace03b943
Honti Dániel <honti.daniel03@gmail.com>
2022-05-10 18:08:06 +0200

    Jegyzokonyv, kommenteles, szepites

--- OSSemTask_HR6121/HR6121_1.c
+++ OSSemTask_HR6121/HR6121_1.c
@@ -1,4 +1,5 @@
-
+//6. feladat
+//Beolvas egy random szamot, megduplazza, visszaküldi 
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
@@ -9,37 +10,34 @@
 int main()
 {
     int fd1;
-    int fd2;
 
-
+   
     char * myfifo = "/tmp/myfifo1";
 
-
+    //letrehozza a csovezeteket
     mkfifo(myfifo, 0666);
 
     int number;
     int i;
         
-
+	//Megnyitja a csovezeteket olvasasra, var hogy irjanak bele, kiolvassa belole majd kiirja a szamot, majd bezarja a csovezeteket
         fd1 = open(myfifo,O_RDONLY);
         
         read(fd1, &number, sizeof(int));
 
-     
-        
         printf("Number: %d\n", number);
-
 
         close(fd1);
 
+
+	//megduplazza a szamot
         number = number * 2;
 
+	//Megnyitja a csovezeteket irasra, beleirja a megduplazott szamot, bezarja a csovezeteket
         fd1 = open(myfifo,O_WRONLY);
 
         write(fd1, &number, sizeof(int));
           
-       
-
         close(fd1);
 
     return 0;
--- OSSemTask_HR6121/HR6121_2.c
+++ OSSemTask_HR6121/HR6121_2.c
@@ -1,4 +1,5 @@
-
+//6. feladat
+//Beleir egy random szamot a csovezetekbe, majd varja a duplajat a szamnak. Ezutan kitorli a csovezeteket.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,34 +12,35 @@
 int main()
 {
     int fd;
-    int fd2;
 
     char * myfifo = "/tmp/myfifo1";
 
+    //letrehozza a csovezeteket
     mkfifo(myfifo, 0666);
 
+    //general egy random szamot
     int number;
     srand(time(0));
     number = rand()%100 + 1;
     
 
+	//megnyitja a csovezeteket irasra, beleirja a random generalt szamot, majd bezarja 
         fd = open(myfifo, O_WRONLY);
-
-        
-        
+                
         write(fd, &number, sizeof(int));
         
         close(fd);
 
         
-        
+        //megnyitja a csovezeteket olvasasra, var hogy tegyenek bele egy szamot, majd kiirja, becsukja a csovezeteket
         fd = open(myfifo, O_RDONLY);
 
-        
         read(fd, &number, sizeof(int));
 
         printf("Number doubled: %d\n", number);
         close(fd);
+
+	//kitorli a csovezeteket
         remove("/tmp/myfifo1");
 
     return 0;
